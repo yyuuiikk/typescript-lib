e7dd3d664c8823ebcb16cddc26d1d9ef8f723fc7
yyuuiikk <yutaikawa0315@gmail.com>
2024-08-24 16:58:39 +0900

    探索時間を短くするよう修正

--- lib/leetcode/1_Two_Sum.ts
+++ lib/leetcode/1_Two_Sum.ts
@@ -1,51 +1,29 @@
-// todo: 時間がかかりすぎるので最適化の方法を考える
 function twoSum(nums: number[], target: number): number[] {
-    let answer: number[] = [];
+    let numsMap = new Map;
+    nums.forEach((val, key) => {
+        numsMap.set(key, val);
+    })
 
-    const lastIdx = nums.length - 1;
     for (let i = 0; i < nums.length; i++) {
-        let isOk: boolean = false;
-        if (i === lastIdx) {
-            break;
-        }
+        let search = target - nums[i];
+        for (let [mapKey, mapVal] of numsMap) {
+            if (i === mapKey) {
+                continue;
+            }
 
-        answer = [i];
-        let tmp = nums[i];
-        let nextIdx = i + 1;
-        while (lastIdx >= nextIdx) {
-            let flowIdx = nextIdx;
-            while(lastIdx >= flowIdx) {
-                tmp += nums[flowIdx];
-                if (tmp === target) {
-                    isOk = true;
-                    answer.push(flowIdx);
-                    break;
-                } else {
-                    tmp = nums[i];
-                }
-                flowIdx++;
+            if (search === mapVal) {
+                return [i, mapKey];
             }
-            if (isOk) {
-                break;
-            }
-            nextIdx++;
-            tmp = nums[i];
-            answer = [i];
-        }
-        if (isOk) {
-            break;
         }
     }
-    return answer;
+    return [];
 };
 
 // const nums = [3, 2, 3];
-// const nums = [3,2,4];
 // const target = 6;
 // const nums = [0,4,3,0];
 // const target = 0;
 const nums = [1,1,1,1,1,4,1,1,1,1,1,7,1,1,1,1,1];
 const target = 11;
-
 
 console.log(twoSum(nums, target));
