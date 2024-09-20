1a4f8e940535ae4ab9f2669cda5bf037599b8084
yyuuiikk <yutaikawa0315@gmail.com>
2024-09-20 22:58:09 +0900

    回答を追加

---
+++ lib/leetcode/Hash_Table/Jewels_and_Stones.ts
@@ -0,0 +1,13 @@
+function numJewelsInStones(jewels: string, stones: string): number {
+    let count: number = 0;
+    for (let i = 0; i < stones.length; i++) {
+        for (let j = 0; j < jewels.length; j++) {
+            if (stones[i] === jewels[j]) count++;
+        }
+    }
+    return count;
+};
+
+let jewels = "aA";
+let stones = "aAAbbbb";
+console.log(numJewelsInStones(jewels, stones));
